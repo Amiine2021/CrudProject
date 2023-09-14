b236c2e6e773ee11aacf5d96b27fe6ecbf4bed0f
amine <mraminewiebke@gmail.com>
2023-09-14 21:43:23 +0200

    part 4-5

--- main.js
+++ main.js
@@ -8,7 +8,7 @@ let count = document.getElementById('count')
 let category = document.getElementById('category')
 let submit = document.getElementById('submit')
 // console.log(title,price,taxes,ads,discount,total,count,category,submit)
-
+// getTotal
 function getTotal(){
 
 
@@ -26,3 +26,91 @@ function getTotal(){
     }
 
 }
+// create product
+let proData;
+if(localStorage.product != null){
+    proData = JSON.parse(localStorage.product)
+
+} else {
+    proData = []
+}
+
+
+
+
+
+// create input 
+submit.onclick = function(){
+    let getData = {
+    title:title.value,
+    price:price.value,
+    taxes:taxes.value,
+    ads:ads.value,
+    discount:discount.value,
+    total:total.innerHTML,
+    count:count.value,
+    category:category.value,
+
+}
+
+proData.push(getData);
+localStorage.setItem('product', JSON.stringify(proData))
+console.log(proData)
+  clearData();
+  setData()
+}
+
+// clear input 
+function clearData(){
+    title.value = "";
+   price.value = "";
+  taxes.value= '';
+    ads.value = '';
+   discount.value = '';
+    total.innerHTML = '';
+   count.value = '';
+    category.value = '';
+}
+
+// read
+
+function setData(){
+    let table = '';
+
+for( let i = 0 ; i < proData.length; i++){
+    table += `
+    <tr>
+                <td>${i}</td>
+                <td>${proData[i].title}</td>
+                <td>${proData[i].price}</td>
+                <td>${proData[i].taxes}</td>
+                <td>${proData[i].ads}</td>
+                <td>${proData[i].discount}</td>
+                <td>${proData[i].count}</td>
+                <td>${proData[i].category}</td>
+                <td><button id="update">update</button></td>
+                <td><button onclick='deleteData(${i})' id="delete">delete</button></td>
+                </tr>
+    
+    
+    
+    
+    `
+    document.getElementById('tbody').innerHTML = table
+}
+
+
+}
+setData();
+
+
+// delete
+
+
+function deleteData(i){
+  proData.splice(i,1);
+  localStorage.product = JSON.stringify(proData);
+  setData()
+}
+
+
